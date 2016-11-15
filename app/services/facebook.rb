d539c86ef4e677b475e6b34803d74107578da261
Sumeet Jain <sumeet@sumeetjain.com>
2016-11-15 13:25:51 -0600

    solved string to int error

--- app/services/facebook.rb
+++ app/services/facebook.rb
@@ -2,16 +2,24 @@ class Facebook
     def self.get_latest_entry
         app_id = ENV["FB_CLIENT_ID"]
         app_secret = ENV["FB_SECRET_ID"]
-        apiInfo = HTTParty.get("https://graph.facebook.com/274838819236148/posts?fields=message,created_time,attachments{media,subattachments}&access_token=#{ENV["FB_CLIENT_ID"]}|#{ENV["FB_SECRET_ID"]}")
-            time = apiInfo["data"][0]["created_time"]
-            relevant_time = time[11..12]
-            relevant_time = relevant_time.to_i
-            if !(relevant_time < 14) and !(relevant_time > 16)
-                # will need the following data to be added in an entry into the DB
-                 message = apiInfo["data"][0]["message"]
-                 image = apiInfo["data"][0]["attachments"]["data"]["media"]["image"]["src"]
-                 created_at_time = apiInfo["data"][0]["created_time"]
-             end
+
+        if ENV["ACCESS_TOKEN"].blank?
+            apiInfo = HTTParty.get("https://graph.facebook.com/274838819236148/posts?fields=message,created_time,attachments{media,subattachments}&access_token=#{ENV["FB_CLIENT_ID"]}|#{ENV["FB_SECRET_ID"]}")
+        else
+            apiInfo = HTTParty.get("https://graph.facebook.com/274838819236148/posts?fields=message,created_time,attachments{media,subattachments}&access_token=#{ENV["ACCESS_TOKEN"]}")
+        end
+
+        time = apiInfo["data"][0]["created_time"]
+        relevant_time = time[11..12]
+        relevant_time = relevant_time.to_i
+        if !(relevant_time < 14) and !(relevant_time > 16)
+            # will need the following data to be added in an entry into the DB
+             message = apiInfo["data"][0]["message"]
+             binding.pry
+             image = apiInfo["data"][0]["attachments"]["data"][0]["media"]["image"]["src"]
+             created_at_time = apiInfo["data"][0]["created_time"]
+        end
+
         # Call Facebook, check for a new entry
         # entry = ???
 
